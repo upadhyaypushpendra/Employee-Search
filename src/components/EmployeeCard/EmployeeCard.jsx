34bb4e2a70c890b6ec1b7b004997de4de61c15c1
Pushpendra <pushpendra@localhost.localdomain>
2020-11-01 21:06:25 +0530

    Updated EmployeeCard.jsx

--- src/components/EmployeeCard/EmployeeCard.jsx
+++ src/components/EmployeeCard/EmployeeCard.jsx
@@ -4,7 +4,7 @@ import profileIcon from './../../employee.svg'
 export default function EmployeeCard({employee}) {
     return (
         <div className="employee-card" >
-            <img src={ employee && employee.profile_image ? employee.profile_image : profileIcon}/>
+            <img alt={"EmployeeImage"} src={ employee && employee.profile_image ? employee.profile_image : profileIcon}/>
             <p>ID : {employee.employee_name}</p>
             <p>Age: {employee.employee_age}</p>
             <p>Salary : {employee.employee_salary}</p>
